87543d1fdea02d14f8bb6d0d865167e83b37a655
clydehsieh <clydehsieh0601@gmail.com>
2015-06-28 11:22:03 +0800

    Initial Commit

---
+++ UICollectionViewPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionViewPractice
+//
+//  Created by Chin-Hui Hsieh  on 6/28/15.
+//  Copyright (c) 2015 Chin-Hui Hsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollectionViewPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollectionViewPractice
+//
+//  Created by Chin-Hui Hsieh  on 6/28/15.
+//  Copyright (c) 2015 Chin-Hui Hsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
